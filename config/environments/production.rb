afa7024e5fcf633a21c80216783c26e61dc3a05f
yang <yxiangxiao@163.com>
2014-01-08 16:12:25 +0800

    modify production config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Sportdemo::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-   config.assets.precompile += %w( login.js, teacher.js, manager.js )
+   config.assets.precompile += %w['login.js', 'teacher.js', 'manager.js']
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
