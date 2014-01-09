02a1e228384c65506cf6f716c1180ade9e399808
yang <yxiangxiao@163.com>
2014-01-09 16:12:14 +0800

    add studentid to student

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,5 @@
 class Student < ActiveRecord::Base
-  attr_accessible :email, :height, :login_id, :name, :point, :sex, :shclass_id, :weight, :phone
+  attr_accessible :email, :height, :login_id, :name, :point, :sex, :shclass_id, :weight, :phone, :studentid
   has_one :login
   has_one :shclass
 end
