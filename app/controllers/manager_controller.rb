12c9a35dc3ec58c136a532e2140da9d18722d8aa
yang <yxiangxiao@163.com>
2014-03-25 19:10:53 +0800

    add logcontent show

--- app/controllers/manager_controller.rb
+++ app/controllers/manager_controller.rb
@@ -1,6 +1,7 @@
 class ManagerController < ApplicationController
 layout "manager" 
 before_filter :authenticate_manager
+
 def phonegrid
 	respond_to do |format|
 		format.js
@@ -66,12 +67,12 @@ def basestation
 	data=Basestation.offset(params[:start].to_i).limit(params[:limit].to_i)
   	totalcount=Basestation.all.count
   	respond_to do |format| 
- 		format.json { render :json=>{:totalCount=>totalcount, :gridData=>data.collect { |list| {:name=>list.name, :place=>list.place, :status=>list.status ? t(:working) : t(:breaking), :name=>list.name, :ip=>list.ip, :code=>list.code, :id=>list.id, :longitude=>list.longitude, :latitude=>list.latitude } }}}
+ 		format.json { render :json=>{:totalCount=>totalcount, :gridData=>data.collect { |list| {:name=>list.name, :place=>list.place, :status=>list.status ? t(:online) : t(:offline), :name=>list.name, :ip=>list.ip, :code=>list.code, :id=>list.id, :longitude=>list.longitude, :latitude=>list.latitude } }}}
   	end
 end
 
 def basestation_add 
-	Basestation.create(:place=>params[:place], :status=>params[:status]==t(:working) ? true : false, :code=>params[:code], :name=>params[:name], :longitude=>params[:longitude], :latitude=>params[:latitude], :ip=>params[:ip])
+	Basestation.create(:place=>params[:place], :code=>params[:code], :name=>params[:name], :longitude=>params[:longitude], :latitude=>params[:latitude], :ip=>params[:ip])
    	respond_to do |format| 
 		format.json { render :json=>{:success=>true }}
   	end
@@ -79,7 +80,7 @@ end
 
 def basestation_edit
 	
-  	Basestation.find(params[:id]).update_attributes(:place=>params[:place], :status=>params[:status]==t(:working) ? true : false, :code=>params[:code], :name=>params[:name], :longitude=>params[:longitude], :latitude=>params[:latitude], :ip=>params[:ip], :setparamsflag=>'0')
+  	Basestation.find(params[:id]).update_attributes(:place=>params[:place], :code=>params[:code], :name=>params[:name], :longitude=>params[:longitude], :latitude=>params[:latitude], :ip=>params[:ip], :setparamsflag=>'0')
    
 	respond_to do |format| 
    		format.json { render :json=>{:success=>true, :griddata=>Basestation.all }}
@@ -95,10 +96,30 @@ def basestation_delete
 end
 
 def stationlog
-	Basestation.find(params[:stationid]).update_attributes(:reqlogflag=>'0')
-	respond_to do |format|
-		format.json { render :json=>{:gridData=>Basestation.find(params[:stationid]) }}
+	@basestation=Basestation.find(params[:stationid])
+	@basestation.update_attributes(:reqlogflag=>'0')
+=begin
+	count=1
+	flag=true
+	for i in 0..5
+		sleep(1)
+		count=count+1
+   		if @basestation.reqlogflag=='1' then
+      		break
+   		end
 	end
+	if count>3
+		flag=false
+    end
+=end
+	sleep(3)
+	respond_to do |format|
+		format.json { render :json=>{:gridData=>getStationLog(params[:stationid]) }}
+	end
+end
+
+def getStationLog(stationid)
+	return Basestation.find(stationid)
 end
 
 end
